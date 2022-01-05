800054d0c810d5b762c937c1def3610ee7b5751d
unknown <Mario@PCPOSADA.metrovias.com.ar>
2022-01-05 14:26:47 -0300

    Ejecicio 1 Go

---
+++ Exercises/myWeight.go
@@ -0,0 +1,14 @@
+// 2022 - Creado por Mario Posada
+
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("My weight on surface of Mars is ")
+	fmt.Print(92*0.3873)
+	fmt.Print(" Kg, and Y would be ")
+	fmt.Print(43*365/687)
+	fmt.Print(" years old.")
+}
